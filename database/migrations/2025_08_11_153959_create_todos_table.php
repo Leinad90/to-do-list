6eaca4fa7120bae022d4c5af32890320b41fe3a2
Leinad90 <daniel.hejduk@gmail.com>
2025-08-07 16:27:35 +0200

    Testing - allow empty destription

--- database/migrations/2025_08_11_153959_create_todos_table.php
+++ database/migrations/2025_08_11_153959_create_todos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('title');
-            $table->longText('description');
+            $table->longText('description')->nullable()->default(null);
             $table->boolean('completed')->default(false);
         });
     }
