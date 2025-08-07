864b3b5b4a075676686ec830076cf251b54fc23d
Leinad90 <daniel.hejduk@gmail.com>
2025-08-07 16:23:38 +0200

    phpstan

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,39 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\View;
 
 class TodoController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         $todos = Todo::all();
         return view('todos.index', compact('todos'));
     }
 
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        $request->validate(['title' => 'required']);
+        $request->validate(['title' => ['required','string']]);
         $data = $request->all();
         unset($data['_token']);
-        Todo::create($data);
+        Todo::create($data); //@phpstan-ignore argument.type
         return redirect()->back();
     }
 
-    public function setDone(Request $request)
+    public function setDone(Request $request): RedirectResponse
     {
+        $request->validate(['id' => ['required','integer']]);
         $a = DB::table('todos')->where('id','=', $request->input('id'))->update(['completed' => 1]);
         var_dump($a);
         return redirect()->back();
     }
 
-    public function destroy(Request $request)
+    public function destroy(Request $request): RedirectResponse
     {
-        $request->validate(['id' => 'required']);
-        Todo::destroy($request->get('id'));
+        $request->validate(['id' => ['required','integer']]);
+        Todo::destroy((int)$request->get('id')); //@phpstan-ignore cast.int
         return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TodoController;
